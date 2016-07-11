df6d13abed367f71054da03f36e4d42ae2afd40a
Aaron Bini <bini.aaron@gmail.com>
2016-07-11 15:49:00 -0700

    fixed the hello-cli-test

--- test/hello-cli-test.js
+++ test/hello-cli-test.js
@@ -3,7 +3,7 @@ var exec = require('child_process').execSync;
 
 describe('command line interface', function(){
   it('says hello to name entered in cli', function(){
-    var result = exec('node hello-cli.js Aaron').toString().replace('\n', '');
-    assert.equal(result, 'Hello Aaron!');
+    var result = exec('node hello-cli.js Aaron').toString()/*.replace('\n', '')*/;
+    assert.equal(result, 'Hello Aaron!\n');
   });
 });
