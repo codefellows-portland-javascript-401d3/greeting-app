3547f41ad5761559e2e5917107214a1c67e9d848
Kate Sowles <katesowles@gmail.com>
2016-07-12 14:52:27 -0700

    1.2.0 release, removed what-dog, added option to omit parameter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,18 @@
 # CHANGE LOG
 
+## [1.2.0] - 2016-07-11
+#### Added
+- Integration with dog-ascii-faces, dog-names, and emoji-dog libraries.
+- Ability to decide whether or not you enter a 'name' parameter to the function, greeting will differ based on the decision both via node and the CLI prompt.
+
+#### Removed
+- What-dog library, no longer applicable.
+
+---
+
 ## [1.1.0] - 2016-07-11
 #### Added
-- Added dog-ascii-faces, what-dog, and emoji-dog, and dog-names NPM libraries to app, integrations to come.
+- Dog-ascii-faces, what-dog, and emoji-dog, and dog-names NPM libraries to app, integrations to come.
 
 ---
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,61 @@
-var exec = require('child_process').execSync;
-var assert = require('assert');
-var greet = require('../greet');
+const exec = require('child_process').execSync;
+const assert = require('chai').assert;
+const greet = require('../greet');
+const emoji = require('emoji-dog');
+const names = require('dog-names');
+const ascii = require('dog-ascii-faces');
 
-describe ('testing output', () => {
-  it ('- specific', () => {
-    assert.equal(greet('kate'), 'hello, kate');
+describe ('input / output checks', () => {
+  describe ('- specific input', () => {
+    it ('- test 1/1', () => {
+      const greeting = greet('kate');
+      assert.equal(greeting, `${emoji}  hello there kate. Sorry to be in such a hurry but I\'ve got to get to the park and sniff a butt. It\'s been great to see you but I\'ve got to be off! Woof woof!`);
+    });
   });
 
-  it ('- no input', () => {
-    assert.equal(greet(), 'hello, ye ol\' mutt!');
+  describe ('- no input', () => {
+    it ('- test 1/4', () => {
+      const greeting = greet();
+      assert.include(greeting, `${emoji}  hello there `);
+    });
+
+    it ('- test 2/4', () => {
+      const greeting = greet();
+      assert.include(greeting, ', or is it ');
+    });
+
+    it ('- test 3/4', () => {
+      const greeting = greet();
+      assert.include(greeting, '? I can never remember');
+    });
+
+    it ('- test 4/4', () => {
+      const greeting = greet();
+      assert.include(greeting, 'Sorry to be in such a hurry but I\'ve got to get to the park and sniff a butt. It\'s been great to see you but I\'ve got to be off! Woof woof!');
+    });
   });
 
-  it ('- CLI(ish)', () => {
-    var result = exec('node index.js kate').toString();
-    assert.equal(result, 'hello, kate\n');
+  describe ('- CLI input', () => {
+    it ('- test 1/1', () => {
+      const greeting = exec('node index.js jacob').toString();
+      assert.equal(greeting, `${emoji}  hello there jacob. Sorry to be in such a hurry but I\'ve got to get to the park and sniff a butt. It\'s been great to see you but I\'ve got to be off! Woof woof!\n`);
+    });
+  });
+});
+
+describe ('integration checks', () => {
+  it ('- dog-names', () => {
+    let name1 = names.allRandom();
+    let name2 = names.allRandom();
+    assert.include(`${names.all}`, `${name1}`, 'please check dog-names integration');
+    assert.include(`${names.all}`, `${name2}`, 'please check dog-names integration');
+  });
+
+  it ('- emoji-dog', () => {
+    assert.deepEqual(`${emoji}`, '🐶', 'please check emoji-dog integration');
+  });
+
+  it ('- dog-ascii-faces', () => {
+    assert.ok(`${ascii}`, 'please check dog-ascii-faces integration');
   });
 });
