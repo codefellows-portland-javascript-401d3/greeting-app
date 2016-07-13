f2d375a4efb64602ebf17648d1940e759c79d31e
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-07-13 07:47:44 -0700

    added emoji feature

--- test/hello.test.js
+++ test/hello.test.js
@@ -2,15 +2,20 @@ var hello = require('../hello');
 var exec = require('child_process').exec;
 var assert = require('assert');
 
-describe( 'hello', () => {
-  it( 'says hello', () => {
+describe('hello', () => {
+  it('says hello', () => {
     var result = hello('Jane');
-    if (result !== 'Hello, Jane') throw new Error('Add method failed test!');
+    if (result !== 'Hello, Jane') throw new Error('Hello method failed test!');
+  });
+
+  it('displays an emoji at the end', () => {
+    var result = hello('Jane','heart');
+    if (result !== 'Hello, Jane❤️') throw new Error('Hello with emoji method failed test!');
   });
 });
 
-describe( 'greet.js', () => {
-  it( 'processes arguments correctly', (done) => {
+describe('greet.js', () => {
+  it('processes arguments correctly', (done) => {
     var name = 'Bob';
     exec('node greet.js ' + name, (error, stdout, stderr) => {
       if(error) {
